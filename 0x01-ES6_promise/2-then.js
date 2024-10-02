789b05a7411305642b4e2902bcaca306435071f6
MarwaneMLE <khmarwane10@gmail.com>
2024-10-02 23:40:04 +0000

    task then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
+    .catch((error) => {
+      console.error('Error occurred:', error);
+      return new Error('An error occurred while handling the API response.');
+    })
     .finally(() => console.log('Got a response from the API'));
 }
